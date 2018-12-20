d56ee1eeb11979e5d039bbf26d22a1823839fa26
Chayan Bansal <bansalc10@gmail.com>
2018-12-20 17:24:18 +0530

    Signed-off-by: Chayan Bansal <bansalc10@gmail.com>

--- student-analytics-sis-django/studentanalyticssis/settings.py
+++ student-analytics-sis-django/studentanalyticssis/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users.apps.UsersConfig'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,11 @@ WSGI_APPLICATION = 'studentanalyticssis.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'sis_user_db',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1'
     }
 }
 
