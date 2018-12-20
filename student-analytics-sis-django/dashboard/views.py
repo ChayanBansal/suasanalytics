d2d341d80a561fbb2219f06c5c344d50fc5f63a2
Chayan Bansal <bansalc10@gmail.com>
2018-12-20 18:50:09 +0530

    Signed-off-by: Chayan Bansal <bansalc10@gmail.com>

--- student-analytics-sis-django/dashboard/views.py
+++ student-analytics-sis-django/dashboard/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def index_view(request):
     return render(request, 'index.html', {})
 
-
+@login_required
 def home_view(request):
 
     context={}
